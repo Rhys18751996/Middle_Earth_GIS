5e7b35c335ac65118acb148ad8b00136e7e4de89
RhysHorner <18751996@student.westernsydney.edu.au>
2026-06-03 22:58:15 +1000

    starting phase 1

---
+++ UnityProject/Fantasy_World_GIS/Assets/Scripts/Debugging/FlyCameraController.cs
@@ -0,0 +1,137 @@
+using UnityEngine;
+using System;
+using Fantasy_World_GIS.Terrain;
+
+namespace Fantasy_World_GIS.Debugging
+{
+    public class FlyCameraController : MonoBehaviour
+    {
+        [SerializeField]
+        private TerrainStreamingSystem streamingSystem;
+
+        [SerializeField]
+        private float moveSpeed = 150f;
+
+        [SerializeField]
+        private float fastMoveSpeed = 600f;
+
+        [SerializeField]
+        private float rollSpeed = 90f;
+
+        [SerializeField]
+        private float mouseSensitivity = 1f;
+
+        private float yaw;
+        private float pitch;
+
+        private void Start()
+        {
+            Cursor.lockState =
+                CursorLockMode.Locked;
+
+            Cursor.visible = false;
+
+            Vector3 rotation =
+                transform.eulerAngles;
+
+            yaw = rotation.y;
+            pitch = rotation.x;
+        }
+
+        private void Update()
+        {
+            UpdateLook();
+            UpdateRoll();
+            UpdateMovement();
+
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                transform.position =
+                    new Vector3(
+                        300,
+                        450,
+                        300);
+            }
+
+            streamingSystem.UpdateStreaming(transform.position);
+        }
+
+        private void UpdateLook()
+        {
+            float mouseX =
+                Input.GetAxis("Mouse X");
+
+            float mouseY =
+                Input.GetAxis("Mouse Y");
+
+            yaw +=
+                mouseX * mouseSensitivity;
+
+            pitch -=
+                mouseY * mouseSensitivity;
+
+            pitch =
+                Mathf.Clamp(
+                    pitch,
+                    -89f,
+                    89f);
+
+            transform.rotation =
+                Quaternion.Euler(
+                    pitch,
+                    yaw,
+                    0);
+        }
+
+        private void UpdateMovement()
+        {
+            float speed =
+                Input.GetKey(KeyCode.LeftShift)
+                    ? fastMoveSpeed
+                    : moveSpeed;
+
+            Vector3 movement =
+                Vector3.zero;
+
+            if (Input.GetKey(KeyCode.W))
+                movement += transform.forward;
+
+            if (Input.GetKey(KeyCode.S))
+                movement -= transform.forward;
+
+            if (Input.GetKey(KeyCode.A))
+                movement -= transform.right;
+
+            if (Input.GetKey(KeyCode.D))
+                movement += transform.right;
+
+            if (Input.GetKey(KeyCode.Space))
+                movement += Vector3.up;
+
+            if (Input.GetKey(KeyCode.LeftControl))
+                movement += Vector3.down;
+
+            transform.position +=
+                movement.normalized *
+                speed *
+                Time.deltaTime;
+        }
+
+        private void UpdateRoll()
+        {
+            float roll = 0f;
+
+            if (Input.GetKey(KeyCode.Q))
+                roll += rollSpeed;
+
+            if (Input.GetKey(KeyCode.E))
+                roll -= rollSpeed;
+
+            transform.Rotate(
+                0,
+                0,
+                roll * Time.deltaTime,
+                Space.Self);
+        }
+    }
+}
--- UnityProject/Fantasy_World_GIS/Assets/Scripts/Terrain/MoveAcrossChunksTest.cs
+++ UnityProject/Fantasy_World_GIS/Assets/Scripts/Terrain/MoveAcrossChunksTest.cs
@@ -1,46 +1,46 @@
-using UnityEngine;
-using System;
+// using UnityEngine;
+// using System;
 
-namespace Fantasy_World_GIS.Terrain
-{
-    public class MoveAcrossChunksTest : MonoBehaviour
-    {
-        [SerializeField]
-        private TerrainStreamingSystem streamingSystem;
+// namespace Fantasy_World_GIS.Terrain
+// {
+//     public class MoveAcrossChunksTest : MonoBehaviour
+//     {
+//         [SerializeField]
+//         private TerrainStreamingSystem streamingSystem;
 
-        [SerializeField]
-        private float speed = 500f;
+//         [SerializeField]
+//         private float speed = 500f;
 
-        private void Start()
-        {
-            transform.position =
-                new Vector3(
-                    300,
-                    0,
-                    300);
-        }
+//         private void Start()
+//         {
+//             transform.position =
+//                 new Vector3(
+//                     300,
+//                     0,
+//                     300);
+//         }
 
-        private void Update()
-        {
-            float horizontal =
-                Input.GetAxisRaw("Horizontal");
+//         private void Update()
+//         {
+//             float horizontal =
+//                 Input.GetAxisRaw("Horizontal");
 
-            float vertical =
-                Input.GetAxisRaw("Vertical");
+//             float vertical =
+//                 Input.GetAxisRaw("Vertical");
 
-            Vector3 movement =
-                new Vector3(
-                    horizontal,
-                    0,
-                    vertical);
+//             Vector3 movement =
+//                 new Vector3(
+//                     horizontal,
+//                     0,
+//                     vertical);
 
-            transform.position +=
-                movement.normalized *
-                speed *
-                Time.deltaTime;
+//             transform.position +=
+//                 movement.normalized *
+//                 speed *
+//                 Time.deltaTime;
 
-            streamingSystem.UpdateStreaming(
-                transform.position);
-        }
-    }
-}
+//             streamingSystem.UpdateStreaming(
+//                 transform.position);
+//         }
+//     }
+// }
